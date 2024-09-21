f2c67702fafb39dcaea386afd5ea3da8ea4931eb
Raghav Katta <Raghavkatta2005@gmail.com>
2024-09-21 12:23:52 +0530

    this method

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-String.prototype.yell=function(){
-    return `OMG!!! ${this.toUpperCase()}!!!!Arghhhhhh!`
+function hex(r,g,b){
+    return '#' + ((1<<24)+(r<<16)+(g<<8)+b).toString(16).slice(1)/* this function converts colours written in hexadecimal into their rgb values */
 }
-Array.prototype.pop= function(){
-    return `Sorry I want that element I will never pop it off` /* here we have replaced the original pop method with our own pop method */
+/* function to create a color object */
+function makeColor(r,g,b){
+    const color={}/* assigning empty object to color  */
+color.r =r 
+color.g= g
+color.b = b
+color.rgb= function(){
+    console.log(this)/* we are simply printing this */
+
+return color
+}
+
+
+const firstColor= makeColor(35,255,150)
+firstColor.rgb()/* call the rgb to print the color object */
 }
